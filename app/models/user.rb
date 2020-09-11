ec441fdae0ee177671122232f7d3ec56a087c6f7
Ari Nagashima <ari.nagashi0308@gmail.com>
2020-09-11 14:48:39 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true , format: { with: VALID_EMAIL_REGEX }
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{8,32}\z/
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX}
+            
+  has_secure_password
+  
+end
